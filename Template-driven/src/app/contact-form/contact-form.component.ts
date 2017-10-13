f4ed035fbe2fa4bf5c33512736c76ad016bd9b88
bdsipon <sipon.here@gmail.com>
2017-10-14 04:49:36 +1000

    completed the contact form

--- Template-driven/src/app/contact-form/contact-form.component.ts
+++ Template-driven/src/app/contact-form/contact-form.component.ts
@@ -6,9 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent  {
+contactMethods=[
+  {id:1,name:'Email'},
+  {id:2, name:'Phone'}
 
+]
   log(x){
     console.log(x);
+  }
+  submit(f)
+  {console.log(f);
+    
   }
 
 }
