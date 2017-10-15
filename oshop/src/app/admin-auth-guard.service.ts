ca9c2e004febf67a20fe11d3354e51f3256086bb
bdsipon <sipon.here@gmail.com>
2017-10-15 16:34:59 +1000

    updated the authentication

--- oshop/src/app/admin-auth-guard.service.ts
+++ oshop/src/app/admin-auth-guard.service.ts
@@ -4,7 +4,7 @@ import { UserService } from './user.service';
 import { CanActivate } from '@angular/router';
 import { AuthService } from './auth.service';
 import { Injectable } from '@angular/core';
-import 'rxjs/add/operator/switchMap';
+
 import 'rxjs/add/operator/map';
 
 @Injectable()
@@ -13,8 +13,7 @@ export class AdminAuthGuardService implements CanActivate {
   constructor(private auth:AuthService, private UserService:UserService) { }
 
   canActivate():Observable<boolean>{
-    return this.auth.user$
-    .switchMap(user=> this.UserService.get(user.uid))
+    return this.auth.appUser$
      .map(AppUser=>AppUser.isAdmin);
   }
 
